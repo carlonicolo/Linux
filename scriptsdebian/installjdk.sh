8d73aaeaa6acf2cbdfb1d83dbb47b1df362b0ed2
Carlo Nicolò <carlo.nicolo@hotmail.it>
2015-06-17 19:31:22 +0200

    Scritp for installing Oracle jdk

---
+++ scriptsdebian/installjdk.sh
@@ -0,0 +1,17 @@
+mkdir /usr/local/jvm/
+tar -xvf jdk-*.tar.gz
+mv jdk1.8.* jdk1.8.0
+mv jdk1.8.0 /usr/lib/jvm/
+
+update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0/bin/java" 1
+update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0/bin/javac" 1
+update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.8.0/bin/javaws" 1 
+
+update-alternatives --config java
+update-alternatives --config javac
+update-alternatives --config javaws
+
+echo "Fine script, verifico la versione della jdk \n"
+java -version
+
+echo "Ciao"
